08bd897282b28903db781b85f78669b4cc9a948c
Subhasri Loganathan <subhasri.loganathan@syncfusion.com>
2021-11-29 23:32:19 +0530

    test main

---
+++ VaccinationManagement/BeneficiaryInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationManagement
+{
+    class BeneficiaryInfo
+    {
+        private static int autoIncrementRegNo = 1000;
+
+        public int RegistrationNumber { get; set; }
+        public string BeneficiaryName { get; set; }
+        public int Age { get; set; }
+        public int Gender { get; set; }
+        public long PhoneNumber { get; set; }
+        public string City { get; set; }
+
+        public List<VaccineInfo> VaccinationDetails
+        {
+            get; set;
+        }
+
+
+        public BeneficiaryInfo(string name, int age, Gender gender, long phNumber, string city)
+        {
+            this.BeneficiaryName = name;
+            this.Age = age;
+            this.Gender = Gender;
+            this.PhoneNumber = phNumber;
+            this.City = city;
+            this.RegistrationNumber = autoIncrementRegNo++;
+
+        }
+
+
+
+    }
+    /// <summary>
+    ///   To choose gender(Male,Female,Transgender)
+    /// </summary>
+    public enum Gender
+    {
+        Male = 1,
+        Female,
+        Others
+    }
+
+
+}
---
+++ VaccinationManagement/VaccineInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationManagement
+{
+    class VaccineInfo
+    {
+        public VacType VaccineType { get; set; }
+        public string Dosage { get; set; }
+        public DateTime VaccinatedDate { get; set; }
+
+      
+        public VaccineInfo(VacType vaccineType, DateTime vaccinatedDate)
+        {
+            this.VaccineType = VaccineType;
+            this.VaccinatedDate = vaccinatedDate;
+
+
+        }
+
+
+
+
+    }
+    /// <summary>
+    ///     To choose vaccine types(Covaxin,Covishield,Sputnic)
+    /// </summary>>
+    public enum VacType
+    {
+        Co_vaccine = 1,
+        Covishield,
+        Sputnic
+    }
+}
